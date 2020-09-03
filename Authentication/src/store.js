40c165c3edf4041be95fb4693d2e7636125737a0
binh-nguyen <binhnguyen.24hdev@gmail.com>
2020-09-03 11:51:52 +0700

    create project using authentication in vue app

---
+++ Authentication/src/store.js
@@ -0,0 +1,66 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+export default new Vuex.Store({
+  state: {},
+  mutations: {},
+  actions: {
+    // signup({ commit }, authData) {
+    //   axios
+    //     .post('/accounts:signUp?key=AIzaSyD9bww1jZTvP3nHUfh7VxJGT4Z-NaM5t7M', {
+    //       email: authData.email,
+    //       password: authData.password,
+    //       returnSecureToken: true,
+    //     })
+    //     // .then((res) => console.log(res))
+    //     .then((res) => {
+    //       console.log(res);
+    //       commit('authUser', {
+    //         token: res.data.idToken,
+    //         userId: res.data.localId,
+    //       });
+    //     })
+    //     // .catch((error) => console.log(error));
+    //     .catch((error) => {
+    //       if (error.response) {
+    //         console.log(error.response.data);
+    //       } else if (error.request) {
+    //         console.log(error.request);
+    //       } else {
+    //         console.log('Error', error.message);
+    //       }
+    //     });
+    // },
+    // login({ commit }, authData) {
+    //   axios
+    //     .post(
+    //       '/accounts:signInWithPassword?key=AIzaSyD9bww1jZTvP3nHUfh7VxJGT4Z-NaM5t7M',
+    //       {
+    //         email: authData.email,
+    //         password: authData.password,
+    //         returnSecureToken: true,
+    //       }
+    //     )
+    //     // .then((res) => console.log(res))
+    //     .then((res) => {
+    //       console.log(res);
+    //       commit('authUser', {
+    //         token: res.data.idToken,
+    //         userId: res.data.localId,
+    //       });
+    //     })
+    //     // .catch((error) => console.log(error));
+    //     .catch((error) => {
+    //       if (error.response) {
+    //         console.log(error.response.data);
+    //       } else if (error.request) {
+    //         console.log(error.request);
+    //       } else {
+    //         console.log('Error', error.message);
+    //       }
+    //     });
+    // },
+  },
+  getters: {}
+});
