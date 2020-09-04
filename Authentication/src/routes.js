db455ddd6a1a12cd192009a8b187fa4f24560a61
binh-nguyen <binhnguyen.24hdev@gmail.com>
2020-09-04 08:36:12 +0700

    using protecting routes

--- Authentication/src/routes.js
+++ Authentication/src/routes.js
@@ -2,10 +2,20 @@ import Home from "./components/Home.vue";
 import SignIn from "./components/auth/SignIn.vue";
 import SignUp from "./components/auth/SignUp.vue";
 import Dashboard from "./components/dashboard/Dashboard.vue";
-
+import store from "./store";
 export const routes = [
   { path: "/", component: Home },
   { path: "/signUp", component: SignUp },
   { path: "/signIn", component: SignIn },
-  { path: "/dashboard", component: Dashboard }
+  {
+    path: "/dashboard",
+    component: Dashboard,
+    beforeEnter(to, from, next) {
+      if (store.state.idToken) {
+        next();
+      } else {
+        next("/signin");
+      }
+    }
+  }
 ];
